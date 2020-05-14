70b4b015710e8e9fb8c828ea39ad3315017026c7
jp-searching <jpsearching@tesshu.com>
2020-05-14 17:09:50 +0900

    Add Random*ProcessorTest

---
+++ jpsonic-main/src/test/java/org/airsonic/player/service/upnp/processor/RandomSongByArtistUpnpProcessorTest.java
@@ -0,0 +1,113 @@
+/*
+ This file is part of Jpsonic.
+
+ Jpsonic is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Jpsonic is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Jpsonic.  If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright 2019 (C) tesshu.com
+ */
+package org.airsonic.player.service.upnp.processor;
+
+import org.airsonic.player.domain.Artist;
+import org.airsonic.player.domain.MediaFile;
+import org.airsonic.player.domain.MusicFolder;
+import org.airsonic.player.service.search.AbstractAirsonicHomeTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * Test to correct sort inconsistencies.
+ */
+public class RandomSongByArtistUpnpProcessorTest extends AbstractAirsonicHomeTest {
+
+    private static List<MusicFolder> musicFolders;
+
+    {
+        musicFolders = new ArrayList<>();
+        File musicDir = new File(resolveBaseMediaPath.apply("Sort/Pagination/Artists"));
+        musicFolders.add(new MusicFolder(1, musicDir, "Artists", true, new Date()));
+    }
+
+    @Autowired
+    private RandomSongByArtistUpnpProcessor randomSongByArtistUpnpProcessor;
+
+    @Override
+    public List<MusicFolder> getMusicFolders() {
+        return musicFolders;
+    }
+
+    @Before
+    public void setup() throws Exception {
+        setSortStrict(true);
+        setSortAlphanum(true);
+        settingsService.setSortAlbumsByYear(false);
+        populateDatabaseOnlyOnce();
+    }
+
+    @Test
+    public void testGetItemCount() {
+        assertEquals(31, randomSongByArtistUpnpProcessor.getItemCount());
+    }
+
+    @Test
+    public void testGetItems() {
+
+        Map<String, Artist> c = new HashMap<>();
+
+        List<Artist> items = randomSongByArtistUpnpProcessor.getItems(0, 10);
+        items.stream().filter(g -> !c.containsKey(g.getName())).forEach(g -> c.put(g.getName(), g));
+        assertEquals(c.size(), 10);
+
+        items = randomSongByArtistUpnpProcessor.getItems(10, 10);
+        items.stream().filter(g -> !c.containsKey(g.getName())).forEach(g -> c.put(g.getName(), g));
+        assertEquals(c.size(), 20);
+
+        items = randomSongByArtistUpnpProcessor.getItems(20, 100);
+        assertEquals(11, items.size());
+        items.stream().filter(g -> !c.containsKey(g.getName())).forEach(g -> c.put(g.getName(), g));
+        assertEquals(c.size(), 31);
+
+    }
+
+    @Test
+    public void testGetChildSizeOf() {
+        List<Artist> artists = randomSongByArtistUpnpProcessor.getItems(0, 1);
+        assertEquals(1, artists.size());
+        assertEquals(50, randomSongByArtistUpnpProcessor.getChildSizeOf(artists.get(0)));
+    }
+
+    @Test
+    public void testGetChildren() {
+
+        List<Artist> artists = randomSongByArtistUpnpProcessor.getItems(0, 1);
+        assertEquals(1, artists.size());
+
+        Map<String, MediaFile> c = new HashMap<>();
+
+        List<MediaFile> children = randomSongByArtistUpnpProcessor.getChildren(artists.get(0), 0, 10);
+        children.stream().filter(m -> !c.containsKey(m.getArtist())).forEach(m -> c.put(m.getArtist(), m));
+        assertEquals(c.size(), 1);
+
+    }
+
+}
