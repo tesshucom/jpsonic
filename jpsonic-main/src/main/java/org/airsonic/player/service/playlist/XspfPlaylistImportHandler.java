8d2fa7d117aeedc5197f620c2f5ce8c9a8f6dd4c
tesshucom <webmaster@tesshu.com>
2020-07-04 20:34:06 +0900

    Spotbugs(high) : DE_MIGHT_IGNORE
    Signed-off-by: tesshucom <webmaster@tesshu.com>

--- jpsonic-main/src/main/java/org/airsonic/player/service/playlist/XspfPlaylistImportHandler.java
+++ jpsonic-main/src/main/java/org/airsonic/player/service/playlist/XspfPlaylistImportHandler.java
@@ -7,17 +7,22 @@ import chameleon.playlist.xspf.StringContainer;
 import org.airsonic.player.domain.MediaFile;
 import org.airsonic.player.service.MediaFileService;
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class XspfPlaylistImportHandler implements PlaylistImportHandler {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(XspfPlaylistImportHandler.class);
 
     @Autowired
     MediaFileService mediaFileService;
@@ -41,8 +46,9 @@ public class XspfPlaylistImportHandler implements PlaylistImportHandler {
                     try {
                         File file = new File(new URI(location.getText()));
                         mediaFile = mediaFileService.getMediaFile(file);
-                    } catch (Exception ignored) {}
-
+                    } catch (URISyntaxException e) {
+                        LOG.error("Unable to generate URI.", e);
+                    }
                     if (mediaFile == null) {
                         try {
                             File file = new File(sc.getText());
@@ -57,7 +63,9 @@ public class XspfPlaylistImportHandler implements PlaylistImportHandler {
                 StringBuilder errorMsg = new StringBuilder("Could not find media file matching ");
                 try {
                     errorMsg.append(track.getStringContainers().stream().map(StringContainer::getText).collect(Collectors.joining(",")));
-                } catch (Exception ignored) {}
+                } catch (Exception e) {
+                    LOG.error(errorMsg.toString(), e);
+                }
                 errors.add(errorMsg.toString());
             }
         });
