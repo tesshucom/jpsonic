230f77fc771d8e3824e654673e2592eee1f8e013
tesshucom <webmaster@tesshu.com>
2020-12-17 11:00:15 +0900

    PMD(blocker) : ReturnEmptyArrayRatherThanNull
    Signed-off-by: tesshucom <webmaster@tesshu.com>

--- jpsonic-main/src/main/java/org/airsonic/player/filter/ParameterDecodingFilter.java
+++ jpsonic-main/src/main/java/org/airsonic/player/filter/ParameterDecodingFilter.java
@@ -126,7 +126,7 @@ public class ParameterDecodingFilter implements Filter {
                 return decode(values);
             }
 
-            return null;
+            return new String[0];
         }
 
         private String[] decode(String[] values) {
