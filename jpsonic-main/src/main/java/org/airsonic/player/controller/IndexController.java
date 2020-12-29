4f96ae26aa1802bb68909af060d1040cb69b2646
view-ext <viewext@tesshu.com>
2020-12-29 10:06:15 +0900

    Add full reflesh of web page (#807)

--- jpsonic-main/src/main/java/org/airsonic/player/controller/IndexController.java
+++ jpsonic-main/src/main/java/org/airsonic/player/controller/IndexController.java
@@ -7,10 +7,15 @@ import org.airsonic.player.util.LegacyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/index")
@@ -21,14 +26,24 @@ public class IndexController {
     @Autowired
     private SettingsService settingsService;
 
-    @GetMapping
-    public ModelAndView index(HttpServletRequest request) {
+    private Map<String, Object> createDefaultModel(HttpServletRequest request) {
         UserSettings userSettings = settingsService.getUserSettings(securityService.getCurrentUsername(request));
-
-        return new ModelAndView("index", "model", LegacyMap.of(
+        return LegacyMap.of(
                 "autoHidePlayQueue", userSettings.isAutoHidePlayQueue(),
                 "keyboardShortcutsEnabled", userSettings.isKeyboardShortcutsEnabled(),
                 "showLeft", userSettings.isCloseDrawer(),
-                "brand", settingsService.getBrand()));
+                "brand", settingsService.getBrand());
+    }
+
+    @GetMapping
+    public ModelAndView index(HttpServletRequest request) {
+        return new ModelAndView("index", "model", createDefaultModel(request));
+    }
+
+    @PostMapping
+    public ModelAndView index(HttpServletRequest request, @RequestParam("mainView") Optional<String> mainView) {
+        Map<String, Object> model = createDefaultModel(request);
+        mainView.ifPresent(v -> model.put("mainView", v));
+        return new ModelAndView("index", "model", model);
     }
 }
