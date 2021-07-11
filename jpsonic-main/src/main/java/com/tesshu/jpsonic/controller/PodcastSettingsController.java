47f424e0f8cf4e66832a6e21b1a1e35cbe94b4f0
tesshucom <webmaster@tesshu.com>
2021-07-11 09:47:50 +0900

    Fix missing annotations (#1070)

--- jpsonic-main/src/main/java/com/tesshu/jpsonic/controller/PodcastSettingsController.java
+++ jpsonic-main/src/main/java/com/tesshu/jpsonic/controller/PodcastSettingsController.java
@@ -63,7 +63,8 @@ public class PodcastSettingsController {
     }
 
     @PostMapping
-    protected ModelAndView doSubmitAction(@ModelAttribute PodcastSettingsCommand command,
+    protected ModelAndView doSubmitAction(
+            @ModelAttribute(Attributes.Model.Command.VALUE) PodcastSettingsCommand command,
             RedirectAttributes redirectAttributes) {
         settingsService.setPodcastUpdateInterval(Integer.parseInt(command.getInterval()));
         settingsService.setPodcastEpisodeRetentionCount(Integer.parseInt(command.getEpisodeRetentionCount()));
