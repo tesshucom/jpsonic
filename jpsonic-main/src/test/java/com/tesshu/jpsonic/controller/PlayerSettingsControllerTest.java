0d6e0b5c235651145b12efd9467e7550aa2500f7
tesshucom <webmaster@tesshu.com>
2022-02-22 22:30:27 +0900

    Fix to hide options on Player Settings page if useExternalPlayer is false

--- jpsonic-main/src/test/java/com/tesshu/jpsonic/controller/PlayerSettingsControllerTest.java
+++ jpsonic-main/src/test/java/com/tesshu/jpsonic/controller/PlayerSettingsControllerTest.java
@@ -30,6 +30,7 @@ import java.util.concurrent.ExecutionException;
 import com.tesshu.jpsonic.command.PlayerSettingsCommand;
 import com.tesshu.jpsonic.dao.TranscodingDao;
 import com.tesshu.jpsonic.domain.Player;
+import com.tesshu.jpsonic.domain.PlayerTechnology;
 import com.tesshu.jpsonic.domain.Transcoding;
 import com.tesshu.jpsonic.service.PlayerService;
 import com.tesshu.jpsonic.service.SecurityService;
@@ -39,6 +40,7 @@ import com.tesshu.jpsonic.service.ShareService;
 import com.tesshu.jpsonic.service.TranscodingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -49,10 +51,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 class PlayerSettingsControllerTest {
 
+    private SettingsService settingsService;
     private PlayerService playerService;
+    private PlayerSettingsController controller;
     private MockMvc mockMvc;
 
     @BeforeEach
@@ -67,15 +72,14 @@ class PlayerSettingsControllerTest {
         Mockito.when(playerService.getPlayer(Mockito.any(), Mockito.any())).thenReturn(player);
         Mockito.when(playerService.getAllPlayers()).thenReturn(Arrays.asList(player));
 
+        settingsService = mock(SettingsService.class);
         TranscodingDao transcodingDao = mock(TranscodingDao.class);
         List<Transcoding> allTranscodings = transcodingDao.getAllTranscodings();
         TranscodingService transcodingService = mock(TranscodingService.class);
         Mockito.when(transcodingService.getTranscodingsForPlayer(Mockito.any())).thenReturn(allTranscodings);
-
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(new PlayerSettingsController(mock(SettingsService.class), mock(SecurityService.class),
-                        playerService, transcodingService, mock(ShareService.class), mock(OutlineHelpSelector.class)))
-                .build();
+        controller = new PlayerSettingsController(settingsService, mock(SecurityService.class), playerService,
+                transcodingService, mock(ShareService.class), mock(OutlineHelpSelector.class));
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
@@ -120,4 +124,38 @@ class PlayerSettingsControllerTest {
         assertNotNull(result);
     }
 
+    @Test
+    @WithMockUser(username = ServiceMockUtils.ADMIN_NAME)
+    void testUseExternalPlayer() throws Exception {
+
+        Player player = playerService.getPlayer(new MockHttpServletRequest(), new MockHttpServletResponse());
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.get("/" + ViewName.PLAYER_SETTINGS.value())
+                        .param(Attributes.Request.ID.value(), Integer.toString(player.getId())))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        assertNotNull(result);
+        ModelAndView modelAndView = result.getModelAndView();
+        assertEquals("playerSettings", modelAndView.getViewName());
+        PlayerSettingsCommand command = (PlayerSettingsCommand) modelAndView.getModelMap()
+                .get(Attributes.Model.Command.VALUE);
+        assertNotNull(command);
+
+        command.setPlayerTechnology(PlayerTechnology.EXTERNAL);
+        ArgumentCaptor<Player> playerCaptor = ArgumentCaptor.forClass(Player.class);
+        Mockito.doNothing().when(playerService).updatePlayer(playerCaptor.capture());
+        controller.doSubmitAction(command, Mockito.mock(RedirectAttributes.class));
+        Mockito.verify(playerService, Mockito.times(1)).updatePlayer(Mockito.any(Player.class));
+        // If useExternalPlayer is false, the initial value will be registered
+        assertEquals(PlayerTechnology.WEB, playerCaptor.getValue().getTechnology());
+
+        Mockito.clearInvocations(playerService);
+        // If true
+        Mockito.when(settingsService.isUseExternalPlayer()).thenReturn(true);
+        playerCaptor = ArgumentCaptor.forClass(Player.class);
+        Mockito.doNothing().when(playerService).updatePlayer(playerCaptor.capture());
+        controller.doSubmitAction(command, Mockito.mock(RedirectAttributes.class));
+        Mockito.verify(playerService, Mockito.times(1)).updatePlayer(Mockito.any(Player.class));
+        // Input from the web page will be registered
+        assertEquals(PlayerTechnology.EXTERNAL, playerCaptor.getValue().getTechnology());
+    }
 }
