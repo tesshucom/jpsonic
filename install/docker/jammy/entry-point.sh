454655b7daf8d42743b66b618d4bdbd4a5da5ec5
tesshucom <webmaster@tesshu.com>
2022-07-27 22:09:26 +0900

    Add Dockerfile for jammy

---
+++ install/docker/jammy/entry-point.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+export LANG="$LANG"
+ln -sf /usr/share/zoneinfo/"$TIME_ZONE" /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+groupname=$(getent group "$GROUP_ID" | cut -d":" -f1)
+if [ -z "$groupname" ]; then
+    groupname=jpsonic
+    addgroup -g "$GROUP_ID" -S $groupname
+fi
+
+username=$(getent passwd "$USER_ID" | cut -d":" -f1)
+if [ -z "$username" ]; then
+    username=jpsonic
+	adduser \
+	    -u "$USER_ID" \
+	    --disabled-password \
+	    --gecos "" \
+	    --home "$JPSONIC_DIR" \
+	    --ingroup "$groupname" \
+	    jpsonic
+fi
+
+addgroup "$username" "$groupname"
+
+if "$SHOW_DATA_DIR"; then
+    ls -n "$JPSONIC_DIR"/data
+fi
+
+gosu "$username":"$groupname" mkdir -p "$JPSONIC_DIR"/data/transcode
+if [ ! -e "$JPSONIC_DIR"/data/transcode/ffmpeg ]; then
+    gosu "$username":"$groupname" ln -fs /usr/bin/ffmpeg "$JPSONIC_DIR"/data/transcode/ffmpeg
+fi
+
+if [[ $# -lt 1 ]] || [[ ! "$1" == "java"* ]]; then
+
+    java_opts_array=()
+    while IFS= read -r -d '' item; do
+        java_opts_array+=( "$item" )
+    done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
+
+    # su-exec and gosu may obscure exit codes.
+    # Use set to enable graceful shutdown.
+    set -- gosu "$username":"$groupname" java \
+     -Dserver.host=0.0.0.0 \
+     -Dserver.port="$JPSONIC_PORT" \
+     -Dserver.contextPath="$CONTEXT_PATH" \
+     -Djpsonic.home="$JPSONIC_DIR"/data \
+     -Djpsonic.defaultMusicFolder="$JPSONIC_DIR"/music \
+     -Djpsonic.defaultPodcastFolder="$JPSONIC_DIR"/podcasts \
+     -Djpsonic.defaultPlaylistFolder="$JPSONIC_DIR"/playlists \
+     -DUPNP_PORT="$UPNP_PORT" \
+     -Dspring.main.banner-mode="$BANNER_MODE" \
+     -Djava.awt.headless=true \
+     "${java_opts_array[@]}" \
+     -jar jpsonic.war "$@"
+fi
+
+exec "$@"
