74489ff17e0689517908a2555d7beb66feeb7d32
tesshucom <webmaster@tesshu.com>
2022-12-01 23:35:40 +0900

    Add WritableMediaFileService
     - Move methods that may conflict with scanning into this class
     - Most of its visibility will be changed to package-private
     - Some methods will remain public but will be changed to conflict-free implementations

---
+++ jpsonic-main/src/main/java/com/tesshu/jpsonic/service/scanner/WritableMediaFileService.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of Jpsonic.
+ *
+ * Jpsonic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jpsonic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * (C) 2022 tesshucom
+ */
+
+package com.tesshu.jpsonic.service.scanner;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Class for writing MediaFile. Package-private methods will be used from the scanning thread. Public methods may be
+ * used from outside the scanning thread, so concurrency will need to be taken care of.
+ */
+@Service
+public class WritableMediaFileService {
+
+}
