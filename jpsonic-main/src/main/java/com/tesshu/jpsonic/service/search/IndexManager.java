aba1248fd0133dfce119a9f7058325bf0687a325
tesshucom <webmaster@tesshu.com>
2023-12-05 22:15:29 +0900

    Bump IndexManager#INDEX_VERSION from 26 to 27
    With this update, previous search index data will no longer be
    available. Full scan will rebuild them with the latest codecs.
    
     - Because the codec is updated and Lucene95 is removed
     - Backward compatibility can be ensured by relying on
       lucene-backward-codecs.jar. (Jpsonic doesn't do that)

--- jpsonic-main/src/main/java/com/tesshu/jpsonic/service/search/IndexManager.java
+++ jpsonic-main/src/main/java/com/tesshu/jpsonic/service/search/IndexManager.java
@@ -94,7 +94,7 @@ public class IndexManager {
      * of AnalyzerFactory, DocumentFactory or the class that they use.
      *
      */
-    private static final int INDEX_VERSION = 26;
+    private static final int INDEX_VERSION = 27;
 
     /**
      * Literal name of index top directory.
