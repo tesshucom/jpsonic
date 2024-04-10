171b526f71542f3e61d4da0e20366ab12fd16127
tesshucom <webmaster@tesshu.com>
2024-04-10 18:40:10 +0900

    Remove unnecessary Settings Properties
     - These have been changed to be managed on the database.

--- jpsonic-main/src/main/java/com/tesshu/jpsonic/service/SettingsService.java
+++ jpsonic-main/src/main/java/com/tesshu/jpsonic/service/SettingsService.java
@@ -99,8 +99,12 @@ public class SettingsService implements ReadWriteLockSupport {
             "database.config.jndi.name", "database.usertable.quote", "ShowJavaJukebox", "AnonymousTranscoding",
             "UseSonos", "SearchMethodLegacy", "SearchMethodChanged", "FastCacheEnabled", "UseRefresh", "ShowRefresh",
             "VerboseLogStart", "VerboseLogScanning", "VerboseLogPlaying", "VerboseLogShutdown",
-            "IgnoreFileTimestampsNext", "FileModifiedCheckSchemeName", "IgnoreFileTimestampsForEachAlbum",
-            "BufferSize");
+            "IgnoreFileTimestampsNext", "FileModifiedCheckSchemeName", "IgnoreFileTimestampsForEachAlbum", "BufferSize",
+            "DlnaIndexVisible", "DlnaIndexId3Visible", "DlnaFolderVisible", "DlnaArtistVisible",
+            "DlnaArtistByFolderVisible", "DlnaAlbumVisible", "DlnaPlaylistVisible", "DlnaAlbumByGenreVisible",
+            "DlnaSongByGenreVisible", "DlnaRecentAlbumVisible", "DlnaRecentAlbumId3Visible", "DlnaRandomSongVisible",
+            "DlnaRandomAlbumVisible", "DlnaRandomSongByArtistVisible", "DlnaRandomSongByFolderArtistVisible",
+            "DlnaPodcastVisible");
 
     private static final int ELEMENT_COUNT_IN_LINE_OF_THEME = 2;
 
@@ -1238,166 +1242,6 @@ public class SettingsService implements ReadWriteLockSupport {
 
     public void setUriWithFileExtensions(boolean b) {
         setProperty(SettingsConstants.UPnP.Basic.URI_WITH_FILE_EXTENSIONS, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaIndexVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.INDEX);
-    }
-
-    @Deprecated
-    public void setDlnaIndexVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.INDEX, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaIndexId3Visible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.INDEX_ID3);
-    }
-
-    @Deprecated
-    public void setDlnaIndexId3Visible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.INDEX_ID3, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaFolderVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.FOLDER);
-    }
-
-    @Deprecated
-    public void setDlnaFolderVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.FOLDER, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaArtistVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.ARTIST);
-    }
-
-    @Deprecated
-    public void setDlnaArtistVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.ARTIST, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaArtistByFolderVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.ARTIST_BY_FOLDER);
-    }
-
-    @Deprecated
-    public void setDlnaArtistByFolderVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.ARTIST_BY_FOLDER, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaAlbumVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.ALBUM);
-    }
-
-    @Deprecated
-    public void setDlnaAlbumVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.ALBUM, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaPlaylistVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.PLAYLIST);
-    }
-
-    @Deprecated
-    public void setDlnaPlaylistVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.PLAYLIST, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaAlbumByGenreVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.ALBUM_BY_GENRE);
-    }
-
-    @Deprecated
-    public void setDlnaAlbumByGenreVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.ALBUM_BY_GENRE, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaSongByGenreVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.SONG_BY_GENRE);
-    }
-
-    @Deprecated
-    public void setDlnaSongByGenreVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.SONG_BY_GENRE, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaRecentAlbumVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.RECENT_ALBUM);
-    }
-
-    @Deprecated
-    public void setDlnaRecentAlbumVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.RECENT_ALBUM, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaRecentAlbumId3Visible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.RECENT_ALBUM_ID3);
-    }
-
-    @Deprecated
-    public void setDlnaRecentAlbumId3Visible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.RECENT_ALBUM_ID3, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaRandomSongVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.RANDOM_SONG);
-    }
-
-    @Deprecated
-    public void setDlnaRandomSongVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.RANDOM_SONG, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaRandomAlbumVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.RANDOM_ALBUM);
-    }
-
-    @Deprecated
-    public void setDlnaRandomAlbumVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.RANDOM_ALBUM, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaRandomSongByArtistVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.RANDOM_SONG_BY_ARTIST);
-    }
-
-    @Deprecated
-    public void setDlnaRandomSongByArtistVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.RANDOM_SONG_BY_ARTIST, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaRandomSongByFolderArtistVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.RANDOM_SONG_BY_FOLDER_ARTIST);
-    }
-
-    @Deprecated
-    public void setDlnaRandomSongByFolderArtistVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.RANDOM_SONG_BY_FOLDER_ARTIST, b);
-    }
-
-    @Deprecated
-    public boolean isDlnaPodcastVisible() {
-        return getBoolean(SettingsConstants.UPnP.Processor.PODCAST);
-    }
-
-    @Deprecated
-    public void setDlnaPodcastVisible(boolean b) {
-        setProperty(SettingsConstants.UPnP.Processor.PODCAST, b);
     }
 
     public boolean isDlnaGenreCountVisible() {
